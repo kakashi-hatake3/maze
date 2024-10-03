ff7a8487b8c69801dd2bfcfaafc5a6583afd0fd6
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-04 02:35:51 +0300

    первый работающий пример лабиринта

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import logging
 import platform
 
+from src.recursive_backtracker import RecursiveBacktrackerField
+
 logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -9,6 +11,10 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     logger.info(platform.python_version())
 
+    field = RecursiveBacktrackerField(10, 6)
+    field.generate_field()
+    print(field)
+    print(f"Start: {field.start.index}")
 
 if __name__ == "__main__":
     main()
