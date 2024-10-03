95e1c596f3985793321c1b1cd673bdba8669dfa8
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-03 20:02:39 +0300

    дополнил класс Cell параметрами соседи и внешняя сторона

--- src/cell.py
+++ src/cell.py
@@ -1,8 +1,41 @@
 class Cell:
-    def __init__(self):
-        self.index: int
+    def __init__(self, width: int, height: int):
+        self.index: int = 0
         self.name: str = 'wall'
         self.road_quality: str = 'normal'
+        self.is_visited: bool = False
+        self.is_external: bool = False
+        self.neighbors = []
+        if self.is_external:
+            # self.external_side: str = ''
+            self.external_side = []
+            if self.index < width:
+                self.external_side.append('down')
+            if self.index >= width * (height - 1):
+                self.external_side.append('up')
+            if self.index % width == width - 1:
+                self.external_side.append('right')
+            if self.index % width == 0:
+                self.external_side.append('left')
+
+    def add_neighbors(self, field: 'Field'):
+        base_sides = ['down', 'up', 'right', 'left']
+        matrix = field.matrix
+        width = field.width
+        if self.is_external:
+            for i in self.external_side:
+                base_sides.remove(i)
+        for side in base_sides:
+            match side:
+                case 'down':
+                    self.neighbors.append(matrix[self.index - width])
+                case 'up':
+                    self.neighbors.append(matrix[self.index + width])
+                case 'right':
+                    self.neighbors.append(matrix[self.index + 1])
+                case 'left':
+                    self.neighbors.append(matrix[self.index - 1])
+
 
     def __str__(self):
         match self.name:
