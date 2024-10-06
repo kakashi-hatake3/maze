eed6a38ced9b5acfc95575f871711bb3707df4d9
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-07 01:29:11 +0300

    добавил новый аттрибут is_this_the_way, который показывает в какие ячейки заходил решатор при поиске финиша

--- src/cell.py
+++ src/cell.py
@@ -6,6 +6,7 @@ class Cell:
         self.index = index
         self.is_finish = False
         self.is_start = False
+        self.is_this_the_way = False
         self.name: str = 'wall'
         self.road_quality: str = 'normal'
         self.is_visited: bool = False
@@ -44,24 +45,28 @@ class Cell:
     def __str__(self):
         match self.name:
             case 'wall':
-                if self.is_finish:
-                    return "\33[32m" + 'F' + "\033[0m"
                 return '#'
             case 'road':
-                if self.is_finish:
-                    return "\33[32m" + 'F' + "\033[0m"
-                if self.is_start:
-                    return "\33[32m" + 'S' + "\033[0m"
                 match self.road_quality:
+                    case 'start':
+                        return "\33[32m" + 'S' + "\033[0m" # 32 - green
+                    case 'finish':
+                        return "\33[32m" + 'F' + "\033[0m" # 32 - green
                     case 'good':
+                        if self.is_this_the_way:
+                            return "\33[34m" + '$' + "\033[0m" # 34 - blue
                         if self.is_visited:
-                            return "\33[35m" + '$' + "\033[0m"
+                            return "\33[35m" + '$' + "\033[0m" # 35 - magenta
                         return '$'
                     case 'bad':
+                        if self.is_this_the_way:
+                            return "\33[34m" + '^' + "\033[0m" # 34 - blue
                         if self.is_visited:
-                            return "\33[35m" + '^' + "\033[0m"
+                            return "\33[35m" + '^' + "\033[0m" # 35 - magenta
                         return '^'
                     case 'normal':
+                        if self.is_this_the_way:
+                            return "\33[34m" + '*' + "\033[0m" # 34 - blue
                         if self.is_visited:
-                            return "\33[35m" + '*' + "\033[0m"
+                            return "\33[35m" + '*' + "\033[0m" # 35 - magenta
                         return '*'
