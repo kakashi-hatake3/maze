b177ad09ca6b71352f1d7f2bb309478b7047fd36
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-06 06:47:16 +0300

    достаточно кривая реализация growing tree

--- src/kruskal_generate.py
+++ src/kruskal_generate.py
@@ -1,3 +1,5 @@
+import random
+
 from src.field import Field
 
 
@@ -6,17 +8,29 @@ class KruskalField(Field):
 
     def __init__(self, width: int, height: int):
         super().__init__(width, height)
-        self.edges = []
-        self.initialize_edges()
+        # self.edges = []
+        # self.initialize_edges()
         # print(self.edges[0][0].index, self.edges[0][1].index)
 
-    def initialize_edges(self):
-        """Инициализирует ребра."""
-        for cell in self.matrix:
-            for neighbour in cell.neighbours:
-                self.edges.append(tuple(sorted((cell, neighbour), key=lambda x: x.index)))
-        self.edges = list(set(self.edges))
+    # def initialize_edges(self):
+    #     """Инициализирует ребра."""
+    #     for cell in self.matrix:
+    #         for neighbour in cell.neighbours:
+    #             self.edges.append(tuple(sorted((cell, neighbour), key=lambda x: x.index)))
+    #     self.edges = list(set(self.edges))
 
     def generate_field(self):
-        """Генерирует поле согласно алгоритму Kruskal."""
-
+        """Генерирует поле согласно алгоритму growing tree."""
+        pretended_cells = []
+        current_cell = self.start
+        while len(pretended_cells) > 0 or current_cell == self.start:
+            current_cell.is_visited = True
+            current_cell.name = 'road'
+            current_cell.road_quality = random.choice(['good', 'bad', 'normal'])
+            for neighbour in current_cell.neighbours:
+                if not neighbour.is_visited:
+                    neighbor_cell_neighbours_status = [c.is_visited for c in neighbour.neighbours]
+                    if neighbor_cell_neighbours_status.count(True) < 2 and neighbour not in pretended_cells:
+                        pretended_cells.append(neighbour)
+            current_cell = random.choice(pretended_cells)
+            pretended_cells.remove(current_cell)
