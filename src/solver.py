609023c1b3a3a5538a36d3a1001f975c21632646
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-07 01:26:36 +0300

    родительский класс для решатора лабиринта

---
+++ src/solver.py
@@ -0,0 +1,19 @@
+class Solver:
+    """Класс для решения лабиринта."""
+
+    def __init__(self, field: 'Field'):
+        self.maze = field
+        self.start = field.start
+        self.finish = field.finish
+        self.finish.is_visited = True
+        self.weights = {
+            'normal': 2,
+            'good': 1,
+            'bad': 3,
+            'start': 0,  # для старта вес можно считать как 0
+            'finish': 0  # для финиша вес не влияет
+        }
+
+    def solve_field(self):
+        """Переопределяемый метод."""
+        pass
