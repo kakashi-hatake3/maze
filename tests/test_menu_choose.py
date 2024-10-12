a881c94a6b1c96c2548a8d31b1c47f3e7c2298c6
kakashi-hatake3 <mayorov.art7@gmail.com>
2024-10-12 20:24:50 +0300

    добавил тесты

--- tests/test_menu_choose.py
+++ tests/test_menu_choose.py
@@ -1,6 +1,3 @@
-import builtins
-from unittest import mock
-
 import pytest
 
 from src.menu_choose import MenuChoose
@@ -11,21 +8,61 @@ def menu_choose():
     return MenuChoose()
 
 
-def test_choose_generate(menu_choose):
-    with pytest.raises(OSError):
-        assert menu_choose.choose_generate() in ["Growing Tree", "Recursive Backtracker"]
+def test_choose_generate_growing_tree(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=[''])
+    result = menu_choose.choose_generate()
+    assert result == "Growing Tree"
 
 
-def test_choose_solve(menu_choose):
-    with pytest.raises(OSError):
-        assert menu_choose.choose_solve() in ["Dijkstra", "Wave Front"]
+def test_choose_generate_recursive_backtracker(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=['s', ''])
+    result = menu_choose.choose_generate()
+    assert result == "Recursive Backtracker"
 
 
-def test_choose_width_and_height_positive(menu_choose):
-    with mock.patch.object(builtins, 'input', lambda _: 5):
-        assert menu_choose.choose_width_and_height() == (5, 5)
+def test_choose_generate_exit(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=['w', ''])
+    result = menu_choose.choose_generate()
+    assert result == "Recursive Backtracker"
 
 
-def test_choose_start_and_finish_positive(menu_choose):
-    with mock.patch.object(builtins, 'input', lambda _: 0):
-        assert menu_choose.choose_start_and_finish() == ((0, 0), (0, 0))
+def test_choose_solve_dijkstra(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=[''])
+    result = menu_choose.choose_solve()
+    assert result == "Dijkstra"
+
+
+def test_choose_solve_wave_front(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=['s', ''])
+    result = menu_choose.choose_solve()
+    assert result == "Wave Front"
+
+
+def test_choose_solve_exit(menu_choose, mocker):
+    mocker.patch('builtins.input', side_effect=['w', ''])
+    result = menu_choose.choose_solve()
+    assert result == "Wave Front"
+
+
+def test_choose_width_and_height_positive(menu_choose, mocker):
+    mocker.patch('os.system')
+    mocker.patch('builtins.input', side_effect=[10, 15])
+    assert menu_choose.choose_width_and_height() == (10, 15)
+
+
+def test_choose_width_and_height_negative(capfd, menu_choose, mocker):
+    mocker.patch('os.system')
+    mocker.patch('builtins.input', side_effect=['asdad', 10, 15])
+    assert menu_choose.choose_width_and_height() == (10, 15)
+
+
+def test_choose_start_and_finish_positive(menu_choose, mocker):
+    mocker.patch('os.system')
+    mocker.patch('builtins.input', side_effect=[0, 1, 2, 0])
+    assert menu_choose.choose_start_and_finish() == ((0, 1), (2, 0))
+
+
+def test_choose_start_and_finish_negative(menu_choose, mocker):
+    mocker.patch('os.system')
+    mocker.patch('builtins.input', side_effect=['asd', 0, 'asd', 2, 0, 'xc', 0, 1, 2, 0])
+    assert menu_choose.choose_start_and_finish() == ((0, 1), (2, 0))
